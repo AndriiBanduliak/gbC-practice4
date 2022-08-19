b4ae1a1563999da99112e085149f7f599d3cb8b2
Andrii Banduliak <banduliak1303@gmail.com>
2022-08-20 00:20:10 +0300

    1st task is done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 25:**Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4-> 16*/
+using static System.Console;
+WriteLine("Enter the number A and the number B");
+int a=getIngeger(); 
+int b=getIngeger();
+WriteLine($"The number A={a} to the power of B={b} is: {pow(a,b)}");
+
+int getIngeger()
+{
+    ///Write("Enter the number: ");
+    return int.Parse(ReadLine());
+}
+int pow(int a, int b)
+{
+    int result = 1;
+    for (int i = 0; i < b; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
