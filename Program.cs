22fae35f29b0af43dc52ef0bd1379edf7f52c914
Andrii Banduliak <banduliak1303@gmail.com>
2022-08-20 00:23:39 +0300

    2nd task is done

--- Program.cs
+++ Program.cs
@@ -1,23 +1,19 @@
-﻿/*Задача 25:**Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-3, 5 -> 243 (3⁵)
-2, 4-> 16*/
-using static System.Console;
-WriteLine("Enter the number A and the number B");
-int a=getIngeger(); 
-int b=getIngeger();
-WriteLine($"The number A={a} to the power of B={b} is: {pow(a,b)}");
+﻿/*Задача 27:** Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
 
-int getIngeger()
+using static System.Console;
+Write("Enter the Number: ");
+WriteLine($"The sum of the digits in the number is: {sum(int.Parse(ReadLine()))}");
+
+int sum(int digit)
 {
-    ///Write("Enter the number: ");
-    return int.Parse(ReadLine());
-}
-int pow(int a, int b)
-{
-    int result = 1;
-    for (int i = 0; i < b; i++)
+    int result = 0;
+    while (digit != 0)
     {
-        result *= a;
+        result+=digit%10;
+        digit /= 10;
     }
     return result;
 }
